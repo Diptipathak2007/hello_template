e2432758ec26d41805f072b94d4be1dea4841efc
Diptipathak2007 <diptipathak019@gmail.com>
2025-10-18 23:19:34 +0530

    feat: add Counter contract and corresponding tests

---
+++ test/Counter.t.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+
+import "src/Counter.sol";
+
+contract TestContract is Test {
+    Counter c;
+    function setUp() public {
+        c = new Counter(5);
+    }
+    function testincrement() public {
+        c.increment();
+        c.increment();
+        assertEq(c.getnum(), 7);
+    }
+    function testdecrement() public {
+        c.decrement();
+        c.decrement();
+        assertEq(c.getnum(), 3);
+    }
+    function testfaildecrement() public {
+        c.decrement();
+        c.decrement();
+        c.decrement();
+        c.decrement();
+        c.decrement();
+
+    }
+}
