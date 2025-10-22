9219c6c6c0c92f26b4986c20f57c45228759f270
Diptipathak2007 <diptipathak019@gmail.com>
2025-10-22 22:27:49 +0530

    update Tiicoin contract to include mint function and adjust test setup

--- src/Tiicoin.sol
+++ src/Tiicoin.sol
@@ -3,7 +3,11 @@ pragma solidity ^0.8.13;
 import {ERC20} from "node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Tiicoin is ERC20 {
-    constructor(uint256 _initialvalue)ERC20("Tiicoin","TII"){
-        _mint(msg.sender,_initialvalue);
+    uint256 public supply;
+    constructor() ERC20("Tiicoin", "TII") {
+        
+    }
+    function mint(address to, uint256 amount) public {
+        _mint(to, amount);
     }
 }
--- test/Tiicoin.t.sol
+++ test/Tiicoin.t.sol
@@ -7,13 +7,15 @@ import "src/Tiicoin.sol";
 
 contract TestTiicoin is Test {
     Tiicoin tiicoin;
-
-
     function setUp() public {
-        tiicoin = new Tiicoin(1000 ether);
+        tiicoin = new Tiicoin();//0.0001
     }
-    function testSimple() public {
-        assertEq(uint(1),uint(1),"1 is not equal to 1");
+    function testMint() public {
+        tiicoin.mint(address(this), 500);
+        assertEq(tiicoin.balanceOf(address(this)), 500, "Balance should be 500 TII");
+        assertEq(tiicoin.balanceOf(0xbF418c1822274015B142715799C24B4Cb576866e), 0, "Address should be zero");
+        tiicoin.mint(address(this), 500);
+        assertEq(tiicoin.balanceOf(address(0xbF418c1822274015B142715799C24B4Cb576866e)), 1000, "Balance should be 1000 TII");
     }
     
 }
